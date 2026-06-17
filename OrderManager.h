909a5450c8254b32647be36cbf963d01e15d1a45
QJ-SB <qijiannan2024@163.com>
2026-06-17 17:26:33 +0000

    docs: fix comment in OrderManager.h

--- OrderManager.h
+++ OrderManager.h
@@ -7,9 +7,9 @@
 
 class OrderManager {
 public:
-    bool add_order(const Order& ord);  //添加订单函数（const引用避免修改和构造）
-    uint64_t get_order_size() const;  //查询订单数函数
-    void print_order() const;         //变量打印订单函数
+    bool add_order(const Order& ord);  //添加订单（const引用避免修改和构造）
+    uint64_t get_order_size() const;  //查询订单数
+    void print_order() const;         //遍历打印订单
     std::optional<Order> get_order_content(uint64_t id) const;  //查询订单内容
     bool update_order_status(uint64_t id, OrderStatus status);  //更新订单状态
     bool apply_fill(uint64_t id, int amount);  //根据撮合结果“扣量 + 判状态”
