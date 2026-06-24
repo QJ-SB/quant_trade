2b031dcb9f361908d081faab50983fcfd6e49ba1
QJ-SB <qijiannan2024@163.com>
2026-06-24 20:24:09 +0000

    style: insert final newline, normalize comment spacing

--- OrderManager.h
+++ OrderManager.h
@@ -7,14 +7,14 @@
 
 class OrderManager {
 public:
-    bool add_order(const Order& ord);  //添加订单（const引用避免修改和构造）
-    uint64_t get_order_size() const;  //查询订单数
-    void print_order() const;         //遍历打印订单
-    std::optional<Order> get_order_content(uint64_t id) const;  //查询订单内容
-    bool update_order_status(uint64_t id, OrderStatus status);  //更新订单状态
-    bool apply_fill(uint64_t id, int amount);  //根据撮合结果“扣量 + 判状态”
+    bool add_order(const Order& ord);  // 添加订单（const引用避免修改和构造）
+    uint64_t get_order_size() const;   // 查询订单数
+    void print_order() const;          // 遍历打印订单
+    std::optional<Order> get_order_content(uint64_t id) const;  // 查询订单内容
+    bool update_order_status(uint64_t id, OrderStatus status);  // 更新订单状态
+    bool apply_fill(uint64_t id, int amount);  // 根据撮合结果“扣量 + 判状态”
 
 private:
     std::unordered_map<uint64_t, Order>
-        m_orders;  //订单管理容器（创建时自动置空）
+        m_orders;  // 订单管理容器（创建时自动置空）
 };
